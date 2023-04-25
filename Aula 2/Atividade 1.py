6e7bfd56df47d858982a785b055f31feb4d986c4
JoaoVargas <jotaguivargas@gmail.com>
2023-04-25 18:16:51 -0300

    Aula 2 adicionada

---
+++ Aula 2/Atividade 1.py
@@ -0,0 +1,13 @@
+a = input("Digite o lado A: ")
+b = input("Digite o lado B: ")
+
+a = int(a)
+b = int(b)
+
+cQuadrado = (a**2) + (b**2)
+
+print("A soma dos quadrados dos catetos é:", cQuadrado)
+
+
+
+
---
+++ Aula 2/Exemplos.py
@@ -0,0 +1,14 @@
+x = 1 	                        # x tem tipo int
+
+x = 2.5                         # x tem tipo float
+
+x = True                        # x tem tipo bool
+
+x = False                       # x tem tipo bool
+
+x = "Escrevi e sai correndo"    # x tem tipo str
+
+
+
+
+
