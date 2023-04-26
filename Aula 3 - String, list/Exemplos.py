a4774a78325311a2e425a3ddad88527352f030db
JoaoVargas <jotaguivargas@gmail.com>
2023-04-26 13:38:27 -0300

    Separado arquivo ex e atv

--- Aula 3 - String, list/Exemplos.py
+++ Aula 3 - String, list/Exemplos.py
@@ -120,29 +120,6 @@ print(n)
 
 
 
-#Atividade 03
-
-listaDesafio1 = ["por", 1234-2211, "parabens", "ERROR", False, False, "responder", False, "ultimo item????????", "não", "esse é o ultimo item"]
-listaDesafio2 = ["corretamente", False ,"desafio", False]
-
-listaDesafio1.pop(-1)
-listaDesafio1.pop(-1)
-listaDesafio1.pop(-1)
-listaDesafio1.remove("ERROR")
-listaDesafio1.remove(False)
-listaDesafio1.remove(False)
-listaDesafio1.remove(False)
-listaDesafio2.remove(False)
-listaDesafio2.remove(False)
-listaDesafio1.pop(1)
-listaDesafio1.sort()
-listaDesafio2.insert(1, "o")
-listaDesafio1.extend(listaDesafio2)
-
-print(listaDesafio1)
-
-
-
 ########################################################################
 #Strings
 #Diferenças entre aspas simples e duplas
